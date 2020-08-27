e436826713650ca6a97be64ce5e188e115611596
sebad-git <sebad-git@users.noreply.github.com>
2020-08-27 05:35:54 -0300

    Fixed

--- vflix/src/app/play-movie/play-movie.component.ts
+++ vflix/src/app/play-movie/play-movie.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { MovieService } from '../../shared/services/movie.service';
 import { Movie } from '../../shared/model/classes';
 
@@ -8,13 +8,16 @@ import { Movie } from '../../shared/model/classes';
   templateUrl: './play-movie.component.html',
   styleUrls: ['./play-movie.component.css']
 })
+
 export class PlayMovieComponent implements OnInit {
 
+  @ViewChild('videoPlayer') videoplayer: ElementRef;
 
   constructor(private movieService: MovieService, private appRoute: ActivatedRoute) { }
 
   public movie:Movie;
   public loaded:boolean;
+  public playing:boolean;
 
   ngOnInit(): void {
     window.scrollTo(0, 0);
@@ -26,6 +29,7 @@ export class PlayMovieComponent implements OnInit {
           this.movieService.findMovie(+paramMovieId).subscribe((data) => {
             this.movie = data;
             this.loaded=true;
+            this.videoplayer.nativeElement.play();
           });
         }
         else { alert("param movie not found"); this.loaded=true; }
